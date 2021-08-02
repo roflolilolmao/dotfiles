889cbfa072aaead4d57289df51e729b895fd6284
roflolilolmao <qjeanmon@gmail.com>
2021-08-02 16:14:16 +0200

    Added nvim-treesitter-textobjects

--- nvim/plugin/nvim-treesitter.lua
+++ nvim/plugin/nvim-treesitter.lua
@@ -21,6 +21,27 @@ require'nvim-treesitter.configs'.setup {
   indent = {
     enable = true,
   },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<Leader>s'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<Leader>S'] = '@parameter.inner',
+      },
+    },
+  },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
