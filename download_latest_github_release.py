ad565e70b0ec100189818cf7870c69c8f2579da6
roflolilolmao <qjeanmon@gmail.com>
2021-08-03 16:40:12 +0200

    Improved download script

--- download_latest_github_release.py
+++ download_latest_github_release.py
@@ -7,17 +7,30 @@ import json
 import tarfile
 import pathlib
 
+from rich import print
+
 url = f'https://api.github.com/repos/{sys.argv[1]}/releases/latest'
 
 data = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
 
-url = data['assets'][0]['browser_download_url']
-filename = data['assets'][0]['name']
+assets = [asset for asset in data['assets']]
+
+while 1:
+    for i, a in enumerate(assets):
+        print(f'{i + 1}. {a["name"]}')
+
+    try:
+        asset = assets[int(input('What file should we download?\n')) - 1]
+        break
+    except (TypeError, IndexError) as e:
+        print(e)
+
+url = asset['browser_download_url']
+filename = asset['name']
 
 print(f'downloading {url} from {filename}')
 
 path = pathlib.Path.home() / '.local/bin'
-
 with urllib.request.urlopen(url) as response:
     with tarfile.open(fileobj=io.BytesIO(response.read())) as tar:
         tar.extractall(path)
