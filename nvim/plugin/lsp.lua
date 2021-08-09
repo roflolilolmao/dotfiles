297e6c11c88d5c2ff383d2ee65c9a5b46e248281
roflolilolmao <qjeanmon@gmail.com>
2021-08-09 10:28:18 +0200

    Cleaned stuff I don't use

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,12 +1,3 @@
-vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
-vim.lsp.handlers['textDocument/references'] = require'lsputil.locations'.references_handler
-vim.lsp.handlers['textDocument/definition'] = require'lsputil.locations'.definition_handler
-vim.lsp.handlers['textDocument/declaration'] = require'lsputil.locations'.declaration_handler
-vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
-vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
-vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
-vim.lsp.handlers['workspace/symbol'] = require'lsputil.symbols'.workspace_handler
-
 Q.lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
 Q.lsp_capabilities.textDocument.completion.completionItem.snippetSupport = true
 Q.lsp_capabilities.textDocument.completion.completionItem.resolveSupport = {
@@ -18,41 +9,52 @@ Q.lsp_capabilities.textDocument.completion.completionItem.resolveSupport = {
 }
 
 Q.lsp_on_attach = function()
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
   local opts = {noremap=true, silent=true}
+
+  local function buf_map(map, func)
+    vim.api.nvim_buf_set_keymap(
+      bufnr,
+      'n',
+      map,
+      [[<Cmd>lua vim.lsp.]] .. func .. [[()<CR>]],
+      opts
+    )
+  end
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
 
-  -- `vim.vim.lsp.buf.range_formatting` might be useful one day.
-  -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  -- buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  -- buf_set_keymap('n', '<Leader>D', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  -- buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-  -- buf_set_keymap('n', '<Leader>fs', '<Cmd> lua vim.lsp.buf.document_symbol()<CR>', opts)
-  -- buf_set_keymap('n', '<Leader>ws', '<Cmd> lua vim.lsp.buf.workspace_symbol()<CR>', opts)
-  -- buf_set_keymap('n', '<Leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  -- Those might be useful one day but are handled by telescope right now
+  -- vim.lsp.buf.range_formatting
+  -- vim.lsp.buf.declaration
+  -- vim.lsp.buf.implementation
+  -- vim.lsp.buf.type_definition
+  -- vim.lsp.buf.definition
+  -- vim.lsp.buf.references
+  -- vim.lsp.buf.document_symbol
+  -- vim.lsp.buf.workspace_symbol
+  -- vim.lsp.buf.code_action
 
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('i', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    'i',
+    '<C-k>',
+    '<Cmd>lua vim.lsp.buf.signature_help()<CR>',
+    opts
+  )
 
-  buf_set_keymap('n', '<Leader>dwa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<Leader>dwr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<Leader>dwl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_map('K', 'buf.hover')
 
-  buf_set_keymap('n', '<Leader>df', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  buf_set_keymap('n', '<Leader>de', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '<Leader>dn', '<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<Leader>dp', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_map('<Leader>dwa', 'buf.add_workspace_folder')
+  buf_map('<Leader>dwr', 'buf.remove_workspace_folder')
+  --buf_map('<Leader>dwl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
 
-  buf_set_keymap('n', '<F2>', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<Leader>q', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_map('<Leader>df', 'buf.formatting')
+  buf_map('<Leader>de', 'diagnostic.show_line_diagnostics')
+  buf_map('<Leader>dn', 'diagnostic.goto_next')
+  buf_map('<Leader>dp', 'diagnostic.goto_prev')
+
+  buf_map('<F2>', 'buf.rename')
+  buf_map('<Leader>q', 'diagnostic.set_loclist')
 end
 
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization
@@ -104,15 +106,6 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] =
     local config = {
       underline = false,
       virtual_text = false,
---      function ()
---        if vim.fn.mode() ~= 'n' then
---          return false
---        end
---        return {
---          prefix = '■ ',
---          spacing = 4,
---        }
---      end,
       signs = true,
       update_in_insert = false,
       severity_sort = true,
