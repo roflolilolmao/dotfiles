74e29978eededc08e43e928da23d2e0b35803da2
roflolilolmao <qjeanmon@gmail.com>
2021-08-11 10:45:46 +0200

    Added nvim-lspinstall

---
+++ nvim/after/plugin/lspinstall.lua
@@ -0,0 +1,51 @@
+-- Configure lua language server for neovim development
+local lua_settings = {
+  Lua = {
+    runtime = {
+      -- LuaJIT in the case of Neovim
+      version = 'LuaJIT',
+      path = vim.split(package.path, ';'),
+    },
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global
+      globals = {'vim'},
+    },
+    workspace = {
+      -- Make the server aware of Neovim runtime files
+      library = {
+        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+      },
+    },
+  }
+}
+
+-- lsp-install
+local function setup_servers()
+  require'lspinstall'.setup()
+
+  -- get all installed servers
+  local servers = require'lspinstall'.installed_servers()
+
+  for _, server in pairs(servers) do
+    local config = {
+      capabilities = Q.lsp_capabilities,
+      on_attach = Q.lsp_on_attach,
+    }
+
+    -- language specific config
+    if server == 'lua' then
+      config.settings = lua_settings
+    end
+
+    require'lspconfig'[server].setup(config)
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd('bufdo e') -- this triggers the FileType autocmd that starts the server
+end
