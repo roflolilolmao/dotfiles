3d7583e8b73c8c4858bd3892107bb98cc6c594e8
roflolilolmao <qjeanmon@gmail.com>
2021-08-25 09:55:25 +0200

    Added zen mode

---
+++ nvim/after/plugin/zen-mode.lua
@@ -0,0 +1,17 @@
+require'zen-mode'.setup{
+  window = {
+    -- height and width can be:
+    -- * an absolute number of cells when > 1
+    -- * a percentage of the width / height of the editor when <= 1
+    -- * a function that returns the width or the height
+    width = 120, -- width of the Zen window
+    height = 0.95, -- height of the Zen window
+    options = {
+      signcolumn = 'yes', -- disable signcolumn
+      number = false, -- disable number column
+      relativenumber = false, -- disable relative numbers
+    },
+  },
+}
+
+Q.m('n', '<F11>', [[<Cmd>ZenMode<CR>]])
