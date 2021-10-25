5ae1b24254ca408b4124ce04beed38774986adc7
roflolilolmao <qjeanmon@gmail.com>
2021-10-25 15:40:43 +0200

    Added nvim-tree

---
+++ nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,16 @@
+require('nvim-tree').setup {
+  diagnostics = {
+    enable = true,
+    icons = {
+      error = Q.lsp.signs.Error,
+      warn = Q.lsp.signs.Warn,
+      info = Q.lsp.signs.Info,
+      hint = Q.lsp.signs.Hint,
+    },
+  },
+  view = {
+    auto_resize = true,
+  },
+}
+
+Q.m("<Leader>'", '<Cmd>NvimTreeToggle<CR>')
